9d500b678bc957a63657ada9fcaf6ab921bce52f
Nikithamboks25 <Nikitha.mbokotwana@capaciti.org.za>
2024-10-10 13:47:28 +0200

    Changes

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -3,7 +3,12 @@ import React from 'react';
 function About(){
     return (
         <div>
-            <h1>This is the about page</h1>
+            <h1>About us</h1>
+            <p>
+                At EduTech we believe whether you're a complete beginner or looking to refresh your skills, 
+                our step-by-step guides and engaging lessons make learning easy and enjoyable.
+                From understanding computer basics to navigating software applications, we have got you covered!
+            </p>
         </div>
 
     );
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,27 @@
 import React from "react";
+import download from "./../Assets/download.jpg"; // Adjust the path as necessary
+import "./Home.css"; 
 
-function Home (){
+function Home() {
     return (
         <div>
-            <h1>This is the Home page</h1>
+        <h1 className="intro-heading">Welcome to EduTech</h1>
+        <p className="intro-paragraph">
+            Unlock your potential with our comprehensive tutorials designed to help you master basic computer skills.
+        </p>
+        <div className="grid-container">
+            <div className="text-section">
+                <h1>What You Can Expect:</h1>
+                <p>Interactive Tutorials: Dive into our user-friendly tutorials tailored for all skill levels.</p>
+                <p>Helpful Resources: Access a wealth of articles, videos, and tips to enhance your learning experience.</p>
+                <p>Community Support: Join our vibrant community of learners and educators, share your experiences, and ask questions.</p>
+            </div>
+            <div className="image-section">
+                <img src={download} alt="Description of the image" />
+            </div>
         </div>
-    )
+        </div>
+    );
 }
+
 export default Home;
